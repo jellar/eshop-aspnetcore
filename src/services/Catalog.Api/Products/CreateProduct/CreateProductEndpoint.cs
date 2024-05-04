45a24b73de6494dc431064569b1653e1898d7872
Rajesh Jella <jella.rajesh@gmail.com>
2024-05-04 08:46:16 +0100

    Create product comand implemented.

---
+++ src/services/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductRequest(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price);
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products",  async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                return new CreateProductResponse(result.Id);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductRequest>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create product")
+            .WithDescription("Create product");
+        }
+    }
+}
